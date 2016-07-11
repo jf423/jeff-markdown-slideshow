fbfdbde624c4d856c565b68777a6fa6f8fa6dbfd
jeff <jf0423@gmail.com>
2016-07-12 00:29:08 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+ 
+  render() {
+    return (
+      <button
+        onClick={() => {
+          this.setState({ count: this.state.count + 1 });
+        }}
+      >
+        Count: {this.state.count}
+      </button>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
